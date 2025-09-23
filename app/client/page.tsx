250d3f929333fd3b03be64a63bcbf79f2cfe04d1
sketchupplaystore-ops <munuvejam@gmail.com>
2025-09-23 22:37:02 +0000

    Add /client route and home links

---
+++ app/client/page.tsx
@@ -0,0 +1,8 @@
+export default function ClientPage() {
+  return (
+    <main className="p-6">
+      <h1 className="text-2xl font-semibold">Client</h1>
+      <p>Client dashboard working.</p>
+    </main>
+  );
+}
